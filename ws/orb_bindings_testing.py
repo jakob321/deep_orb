f74a055490706874eb848f9e76ec57be1d6e0a5a
jakob <jakobmaraton@gmail.com>
2025-02-14 18:05:25 +0100

    test file for orb_slam bindings

--- ws/orb_bindings_testing.py
+++ ws/orb_bindings_testing.py
@@ -7,7 +7,8 @@ import orbslam3
 pathDatasetEuroc="../datasets/EuRoC"
 voc_file = "../ORB_SLAM3/Vocabulary/ORBvoc.txt"
 settings_file = "../ORB_SLAM3/Examples/Monocular/EuRoC.yaml"
+timestamps="../ORB_SLAM3/Examples/Monocular/EuRoC_TimeStamps/MH01.txt"
 
 # Call the minimal SLAM function.
-result = orbslam3.run_orb_slam3(voc_file, settings_file)
+result = orbslam3.run_orb_slam3(voc_file, settings_file,pathDatasetEuroc+"/MH01", timestamps) #
 print(result)
