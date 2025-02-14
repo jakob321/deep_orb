ebe25c8e3d1eae504424a379c59b3ee846fde30c
jakob <jakobmaraton@gmail.com>
2025-02-14 14:29:58 +0100

    updated faulty path

---
+++ ws/orb_bindings_testing.py
@@ -0,0 +1,13 @@
+
+# test.py
+import orbslam3
+
+# Provide paths to the vocabulary and settings files.
+# For a minimal test, you can use dummy paths if you are only verifying the binding.
+pathDatasetEuroc="../datasets/EuRoC"
+voc_file = "../ORB_SLAM3/Vocabulary/ORBvoc.txt"
+settings_file = "../ORB_SLAM3/Examples/Monocular/EuRoC.yaml"
+
+# Call the minimal SLAM function.
+result = orbslam3.run_orb_slam3(voc_file, settings_file)
+print(result)
